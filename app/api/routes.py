dcedca3fdd345e8530863193b03d608843d2be33
Joshua Osgood <osgoodjoshua9@gmail.com>
2024-07-15 22:12:46 -0500

    Updated routes for API

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,6 +3,10 @@ from app import db
 from app.models import Image, Caption
 from app.api import api_bp
 
+# Enable CORS
+from flask_cors import CORS
+CORS(api_bp)
+
 @api_bp.route('/images', methods=['GET'])
 def get_images():
     images = Image.query.all()
@@ -27,3 +31,24 @@ def create_caption():
 def get_caption(caption_id):
     caption = Caption.query.get_or_404(caption_id)
     return jsonify({'id': caption.id, 'text': caption.text, 'image_id': caption.image_id})
+
+@api_bp.route('/captions/<int:caption_id>', methods=['PUT'])
+def update_caption(caption_id):
+    caption = Caption.query.get_or_404(caption_id)
+    data = request.json
+
+    text = data.get('text')
+    if not text:
+        return jsonify({'error': 'Invalid input'}), 400
+
+    caption.text = text
+    db.session.commit()
+
+    return jsonify({'id': caption.id, 'text': caption.text, 'image_id': caption.image_id})
+
+@api_bp.route('/captions/<int:caption_id>', methods=['DELETE'])
+def delete_caption(caption_id):
+    caption = Caption.query.get_or_404(caption_id)
+    db.session.delete(caption)
+    db.session.commit()
+    return '', 204
